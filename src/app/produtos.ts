90dd283414861f8acd1457941a8c35ad707b7f29
rafapcmor <rafapcmor@gmail.com>
2022-05-26 18:12:36 -0300

    2 files modified

--- src/app/produtos.ts
+++ src/app/produtos.ts
@@ -5,14 +5,15 @@ export const produtos = [
     preco: 399,
     descricao:
       'O acessório feminino perfeito para trazer um toque de sofisticação para sua composição de joias de prata. Feito em metal prata 925, formato da pedra central redonda, tipo da pedra zircônia.',
-    image: '/assets/img/anel.png',
+    imagePath:
+      'https://mdbootstrap.com/img/Photos/Horizontal/Nature/4-col/img%20(34).jpg',
   },
   {
     id: 2,
     nome: 'Pulseira',
     preco: 499,
     descricao: 'A pulseira....',
-    imagePath: '/assets/img/pulseira.png',
+    imagePath: 'assets/img/anel.png',
   },
   {
     id: 3,
